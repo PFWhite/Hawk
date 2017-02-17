bd41bb5128d8411b69dd83969c56abb7927deab4
Patrick White <pfwhite@ufl.edu>
2017-02-17 12:33:51 -0500

    finished client side Hawk loader

--- client/app.js
+++ client/app.js
@@ -3,43 +3,48 @@ function () {
     window.Hawk = function (config) {
         var Hawk = function (config) {
             var self = this;
+            this.constructors = {};
+            this.loadStatus = {};
 
-            this.getComponent = function(componentName) {
+            var startLoading = function (componentName) {
                 return new Promise(function (resolve, reject) {
                     var xhr = new XMLHttpRequest();
                     xhr.onreadystatechange = function () {
                         if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
                             resolve(this.response);
+                        } else if (this.status != 200) {
+                            reject(this.response);
                         }
                     };
                     xhr.open('GET', "/components/" + componentName);
+                    self.loadStatus[componentName] = 'loading';
                     xhr.send();
-                }).then(function (response) {
-                    return self.loadComponent(response);
                 });
             };
 
-            this.loadComponent = function (response) {
+            this.getComponent = function(componentName) {
                 return new Promise(function (resolve, reject) {
-                    var scriptTag = window.document.createElement('script');
-                    scriptTag.url = response.codeURL;
-                    window.document.getElementsByTagName('body')[0].appendChild(scriptTag);
-                    self[response.componentName + '_loaded'] = false;
-                    resolve(self.componentOrRetry(response.componentName));
-                });
-            };
-
-            this.componentOrRetry(componentName) {
-                return new Promise(function (resolve, reject) {
-                    if (self[componentName + '_loaded']) {
-                        resolve(self[componentName + '_constructor']);
+                    if (self.loadStatus[componentName] === 'done') {
+                        resolve(self.constructors[componentName]);
                     } else {
-                        window.setTimeout()
+                        reject(componentName);
+                    }
+                })
+                .catch(function (componentName) {
+                    if (self.loadStatus[componentName]) {
+                        return self.getComponent(componentName);
+                    } else {
+                        return startLoading(componentName);
                     }
                 });
-            }
+            };
+
+            this.registerComponent = function (componentName, constructor) {
+                self.constructors[componentName] = constructor;
+                self.loadStatus[componentName] = 'done';
+            };
 
         };
         return new Hawk();
-    }()
-}
+    }();
+}()
