54cc10dbe6e7c378e9ab4e21c71c050addb988a5
Patrick White <pfwhite@ufl.edu>
2017-02-17 14:25:56 -0500

    server running and serving added fake patrick component

---
+++ components/patrick/patrick_test.js
@@ -0,0 +1,5 @@
+describe('patrick Test Cases', function () { 
+    it('Test Case 1', function () {
+       expect(false).toBe(true)
+    });
+});
--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
-express = require('express');
+var express = require('express');
+var path = require('path');
 
 var app = express();
 
+app.use(express.static('/components', express.static(path.join(__dirname, 'components'))));
+
 app.get('/', function (req, res) {
-    var componentName = 'example';
+    var componentName = 'patrick';
     res.send({
-        componentName: 'example',
-        template: '<div>EXAMPLE</div>',
-        codeURL: `/components/${componentName}`
-    })
-})
+        componentName: 'patrick',
+        template: '/components/patrick/patrick.html',
+        codeURL: `/components/patrick/patrick.js`
+    });
+});
+
+app.listen(1337);
